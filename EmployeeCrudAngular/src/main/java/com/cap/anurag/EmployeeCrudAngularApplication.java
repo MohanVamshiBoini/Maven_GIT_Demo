914fcae8d7c840756b59a4f8798350968007f45c
MohanVamshiBoini <boinemohan@gmail.com>
2020-04-13 14:28:56 +0530

    git

---
+++ EmployeeCrudAngular/src/main/java/com/cap/anurag/EmployeeCrudAngularApplication.java
@@ -0,0 +1,13 @@
+package com.cap.anurag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCrudAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCrudAngularApplication.class, args);
+	}
+
+}
